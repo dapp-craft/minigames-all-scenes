833423bfac466b7bb234a86212b57e8377c991a3
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-08-23 21:15:58 -0300

    SlidingPuzzle: Images update

--- SlidingPuzzle/src/game/gameLogic/image.ts
+++ SlidingPuzzle/src/game/gameLogic/image.ts
@@ -1,13 +1,18 @@
-import { lvl1Image } from '../../resources/resources'
+import { lvl3x3Image, lvl4x4Image, lvl5x5Image, lvlAbstractPattern, lvlDCLLogo } from '../../resources/resources'
 import { tileRowColumn } from './tileCalculation'
 
 // Move to separate function to be able to fetch images from the internet if needed or add more logic
 export function getImage(lvl: number) {
-  if ((lvl = 1)) {
-    return lvl1Image
-  }
-
-  return lvl1Image
+  if (lvl == 1) return lvl3x3Image
+  if (lvl == 2) return lvl4x4Image
+  if (lvl == 3) return lvl5x5Image
+  if (lvl == 4) return lvlDCLLogo
+  if (lvl == 5) return lvlDCLLogo
+  if (lvl == 6) return lvlDCLLogo
+  if (lvl == 7) return lvlAbstractPattern
+  if (lvl == 8) return lvlAbstractPattern
+  if (lvl == 9) return lvlAbstractPattern
+  return lvl3x3Image
 }
 
 export function getImageUV(boardSize: number, tileNumber: number): number[] {
