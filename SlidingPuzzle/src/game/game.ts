1e84af493c46d39c61d09ac3ea85eda95df64e90
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-08-28 14:08:06 -0300

    SlidingPuzzle: win anim added

--- SlidingPuzzle/src/game/game.ts
+++ SlidingPuzzle/src/game/game.ts
@@ -1,4 +1,6 @@
 import {
+  Animator,
+  Billboard,
   EasingFunction,
   Entity,
   GltfContainer,
@@ -92,6 +94,8 @@ export async function initGame() {
   initCountdownNumbers()
 
   initStatusBoard()
+
+  setupWinAnimations()
 
   queue.listeners.onActivePlayerChange = (player) => {
     const localPlayer = getPlayer()
@@ -489,13 +493,7 @@ async function finishGame() {
 
   hideAllTiles()
 
-  if (queue.getQueue().length === 1) {
-    const nextLevel = Math.min(gameState.lvl + 1, MAX_LEVEL)
-    gameButtons[nextLevel - 1].enable()
-    startNewLevel(nextLevel)
-  } else {
-    queue.setNextPlayer()
-  }
+  startWinAnimation()
 }
 
 function getLevelSize(level: number): number {
@@ -649,4 +647,163 @@ async function countdown(cb: () => void, number: number) {
     undefined,
     'countdown-system'
   )
+}
+
+
+function setupWinAnimations() {
+  let winAnimA = engine.addEntity()
+  let winAnimB = engine.addEntity()
+  let winAnimC = engine.addEntity()
+  let winAnimFollow = engine.addEntity()
+  let winAnimText = engine.addEntity()
+
+  GltfContainer.create(winAnimA, {
+    src: "mini-game-assets/models/winAnim.glb",
+
+  })
+
+  Transform.create(winAnimA, {
+    parent: sceneParentEntity,
+    position: Vector3.create(0, 3, -6),
+    scale: Vector3.create(1, 1, 1),
+    rotation: Quaternion.fromEulerDegrees(0, 45, 0)
+  })
+
+  Animator.create(winAnimA, {
+    states: [
+      {
+        clip: 'armature_psAction',
+        playing: false,
+        loop: false
+      }
+    ]
+  })
+
+  GltfContainer.create(winAnimB, {
+    src: "mini-game-assets/models/winAnim.glb"
+
+  })
+
+  Transform.create(winAnimB, {
+    parent: sceneParentEntity,
+    position: Vector3.create(0, 3, -6),
+    scale: Vector3.create(1, 1, 1),
+    rotation: Quaternion.fromEulerDegrees(0, 0, 0)
+  })
+
+  Animator.create(winAnimB, {
+    states: [
+      {
+        clip: 'armature_psAction',
+        playing: false,
+        loop: false
+      }
+    ]
+  })
+
+  GltfContainer.create(winAnimC, {
+    src: "mini-game-assets/models/winAnim.glb"
+  })
+
+  Transform.create(winAnimC, {
+    parent: sceneParentEntity,
+    position: Vector3.create(0, 3, -6),
+    scale: Vector3.create(1, 1, 1),
+    rotation: Quaternion.fromEulerDegrees(0, -45, 0)
+  })
+
+  Animator.create(winAnimC, {
+    states: [
+      {
+        clip: 'armature_psAction',
+        playing: false,
+        loop: false
+      }
+    ]
+  })
+
+  GltfContainer.create(winAnimFollow, {
+    src: "mini-game-assets/models/winAnimFollow.glb"
+  })
+
+  Transform.create(winAnimFollow, {
+    parent: sceneParentEntity,
+    position: Vector3.create(0, 3, -6),
+    scale: Vector3.create(0.3, 0.3, 0.3),
+    rotation: Quaternion.fromEulerDegrees(0, -90, 0)
+  })
+  Billboard.create(winAnimFollow, {})
+
+  Animator.create(winAnimFollow, {
+    states: [
+      {
+        clip: 'RaysAnim',
+        playing: false,
+        loop: false
+      }
+    ]
+  })
+
+  GltfContainer.create(winAnimText, {
+    src: "mini-game-assets/models/winAnimText.glb"
+  })
+
+  Animator.create(winAnimText, {
+    states: [
+      {
+        clip: 'Animation',
+        playing: false,
+        loop: false
+      }
+    ]
+  })
+
+  Transform.create(winAnimText, {
+    parent: sceneParentEntity,
+    position: Vector3.create(0, 3, -6),
+    scale: Vector3.create(0.8, 0.8, 0.8),
+    rotation: Quaternion.fromEulerDegrees(0, -90, 0)
+  })
+  Billboard.create(winAnimText, {})
+
+  VisibilityComponent.create(winAnimA, { visible: false })
+  VisibilityComponent.create(winAnimB, { visible: false })
+  VisibilityComponent.create(winAnimC, { visible: false })
+  VisibilityComponent.create(winAnimFollow, { visible: false })
+  VisibilityComponent.create(winAnimText, { visible: false })
+
+  syncEntity(winAnimA, [VisibilityComponent.componentId, Animator.componentId])
+  syncEntity(winAnimB, [VisibilityComponent.componentId, Animator.componentId])
+  syncEntity(winAnimC, [VisibilityComponent.componentId, Animator.componentId])
+  syncEntity(winAnimFollow, [VisibilityComponent.componentId, Animator.componentId])
+  syncEntity(winAnimText, [VisibilityComponent.componentId, Animator.componentId])
+}
+
+
+function startWinAnimation() {
+  const animations = engine.getEntitiesWith(Animator, VisibilityComponent)
+  for (const [entity] of animations) {
+    VisibilityComponent.getMutable(entity).visible = true
+    Animator.getMutable(entity).states[0].playing = true
+  }
+
+  utils.timers.setTimeout(() => {
+
+    const animations = engine.getEntitiesWith(Animator, VisibilityComponent)
+    for (const [entity] of animations) {
+      VisibilityComponent.getMutable(entity).visible = false
+    }
+    console.log("GameData current level: ", GameData.get(gameDataEntity).currentLevel)
+    if (gameState.lvl <= MAX_LEVEL) {
+      // console.log("playersQueue: ", queue.getQueue())
+      //add challenge check
+      if (queue.getQueue().length > 1) {
+        queue.setNextPlayer()
+      } else {
+        const nextLevel = Math.min(gameState.lvl + 1, MAX_LEVEL)
+        gameButtons[nextLevel - 1].enable()
+        startNewLevel(nextLevel)
+      }
+    }
+  }, 8000)
 }
