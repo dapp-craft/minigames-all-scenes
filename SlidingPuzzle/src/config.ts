8bb0b4169c8ea84d93dc82e9d1e97eb1b6c90f29
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-09-11 09:13:11 -0300

    SLidingPuzzle: disable debug UI

--- SlidingPuzzle/src/config.ts
+++ SlidingPuzzle/src/config.ts
@@ -7,6 +7,6 @@ export function setEasyMode(value: boolean) {
   EASY_MODE = value
 }
 
-export const DEBUG_MODE_UI = true
+export const DEBUG_MODE_UI = false
 
 export const SESSION_DURATION = 1000 * 60 * 5 // 5 minutes
