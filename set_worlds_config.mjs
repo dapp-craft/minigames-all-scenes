782023b41b0e0740dbfcf061cc50890e47ebd45f
Maironire <maironire@gmail.com>
2024-09-17 15:16:39 +0300

    test

--- set_worlds_config.mjs
+++ set_worlds_config.mjs
@@ -7,7 +7,7 @@ const FILE_PATH = path.join(process.cwd(), 'scene.json')
 async function setWorldsName() {
   try {
     const jsonData = JSON.parse(await fs.readFile(FILE_PATH, 'utf8'))
-    jsonData.worldConfiguration.name = process.argv[2]
+    jsonData.worldConfiguration = { name: process.argv[2] }
     await fs.writeFile(FILE_PATH, JSON.stringify(jsonData, null, 2))
     console.log(jsonData)
   } catch (error) {
