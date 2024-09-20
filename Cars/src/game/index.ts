6d2c46c72d68bfe1c711c33408603f0cebf85ce9
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-09-20 15:17:34 -0300

    Cars: refactor

--- Cars/src/game/index.ts
+++ Cars/src/game/index.ts
@@ -127,16 +127,26 @@ function setUpInputSystem() {
 }
 
 function processMovement(start: Cell, end: Cell) {
-  if (start == undefined || end == undefined) return
+  if (!start || !end || !inputBuffer.selectedCar) return
   if (start.x === end.x && start.y === end.y) return
-  if (inputBuffer.selectedCar == undefined) return
 
   const car = inputBuffer.selectedCar
   const carData = Car.get(car)
 
+  const availabilityMap = createAvailabilityMap()
+  markCarCellsAsAvailable(availabilityMap, car)
+
+  const movementD = getMovementDelta(start, end, car)
+
+  const finalDelta = calculateFinalDelta(car, movementD, availabilityMap, start)
+
+  Car.getMutable(car).position = {x: carData.position.x + finalDelta.x, y: carData.position.y + finalDelta.y}
+  inputBuffer.startCell = { x: start.x + finalDelta.x, y: start.y + finalDelta.y }
+}
+
+function createAvailabilityMap() {
   const availabilityMap = Array.from({ length: BOARD_SIZE }, () => Array(BOARD_SIZE).fill(0))
 
-  // Mark all cells occupied by cars
   getInGameCars().forEach((car_) => {
     const carData = Car.get(car_)
     const { x: xd, y: yd } = getDirectionVector(carData.direction)
@@ -145,55 +155,65 @@ function processMovement(start: Cell, end: Cell) {
     }
   })
 
-  // Mark cells occupied by the selected car as available
+  return availabilityMap
+}
+
+function markCarCellsAsAvailable(availabilityMap: number[][], car: Entity) {
+  const carData = Car.get(car)
   const { x: xd, y: yd } = getDirectionVector(carData.direction)
   for (let i = 0; i < carData.length; i++) {
     availabilityMap[carData.position.y + yd * i][carData.position.x + xd * i] = 0
   }
+}
 
-  const isPositionAvailable = (cell: Cell) => {
-    const { x: xd, y: yd } = getDirectionVector(carData.direction)
-    for (let i = 0; i < carData.length; i++) {
-      if (cell.y + yd * i < 0 || cell.y + yd * i >= BOARD_SIZE) return false
-      if (cell.x + xd * i < 0 || cell.x + xd * i >= BOARD_SIZE) return false
-      if (availabilityMap[cell.y + yd * i][cell.x + xd * i] == 1) return false
-    }
-    return true
+function getMovementDelta(start: Cell, end: Cell, car: Entity): { x: number; y: number } {
+  const carData = Car.get(car)
+  const delta = {
+    x: end.x - start.x,
+    y: end.y - start.y
   }
+  if (carData.direction === CarDirection.up || carData.direction === CarDirection.down) delta.x = 0
+  if (carData.direction === CarDirection.left || carData.direction === CarDirection.right) delta.y = 0
+  return delta
+}
 
-  const movementD = movementDelta(start, end)
-  if (carData.direction === CarDirection.up || carData.direction === CarDirection.down) movementD.x = 0
-  if (carData.direction === CarDirection.left || carData.direction === CarDirection.right) movementD.y = 0
+function isPositionAvailable(cell: Cell, car: Entity, availabilityMap: number[][]): boolean {
+  const carData = Car.get(car)
+  const { x: xd, y: yd } = getDirectionVector(carData.direction)
+  for (let i = 0; i < carData.length; i++) {
+    const newY = cell.y + yd * i
+    const newX = cell.x + xd * i
+    if (newY < 0 || newY >= BOARD_SIZE || newX < 0 || newX >= BOARD_SIZE || availabilityMap[newY][newX] === 1) {
+      return false
+    }
+  }
+  return true
+}
+
+function calculateFinalDelta(
+  car: Entity,
+  movementD: { x: number; y: number },
+  availabilityMap: number[][],
+  start: Cell
+): Cell {
+  if (!Car.getOrNull(car)) console.error('Car not found')
+  const carData = Car.get(car)
   const distance = Math.abs(movementD.x + movementD.y)
-
-  let validFinalPosition = carData.position
-
+  let finalDelta = { x: 0, y: 0 }
   for (let i = 0; i <= distance; i++) {
-    const xd = movementD.x == 0 ? 0 : Math.sign(movementD.x)
-    const yd = movementD.y == 0 ? 0 : Math.sign(movementD.y)
+    const xd = movementD.x === 0 ? 0 : Math.sign(movementD.x)
+    const yd = movementD.y === 0 ? 0 : Math.sign(movementD.y)
     const possiblePosition: Cell = {
       x: carData.position.x + xd * i,
       y: carData.position.y + yd * i
     }
-    
 
-    if (isPositionAvailable(possiblePosition)) {
-      validFinalPosition = possiblePosition
-      inputBuffer.startCell = {
-        x: start.x + xd * i,
-        y: start.y + yd * i
-      }
+    if (isPositionAvailable(possiblePosition, car, availabilityMap)) {
+      finalDelta = { x: xd * i, y: yd * i }
     } else {
       break
     }
   }
-  Car.getMutable(car).position = validFinalPosition
-}
 
-function movementDelta(start: Cell, end: Cell) {
-  // TODO: movement only in direvtion of the car
-  return {
-    x: end.x - start.x,
-    y: end.y - start.y
-  }
+  return finalDelta
 }
