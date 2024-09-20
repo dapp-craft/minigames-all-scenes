6ccf06f195e35956e538ca1d8847eb4ecf47eec8
Maironire <maironire@gmail.com>
2024-09-20 13:39:33 +0300

    SpotTheDifference: rework levels

--- SpotTheDifference/src/game/levels.ts
+++ SpotTheDifference/src/game/levels.ts
@@ -1,5 +1,8 @@
 export const LEVELS = {
     1: {difficulty: {easy: 5, medium: 0, hard: 0}, total: 15, goal: 3},
     2: {difficulty: {easy: 3, medium: 3, hard: 0}, total: 20, goal: 4},
-    3: {difficulty: {easy: 0, medium: 3, hard: 3}, total: 30, goal: 5}
+    3: {difficulty: {easy: 1, medium: 4, hard: 1}, total: 25, goal: 4},
+    4: {difficulty: {easy: 1, medium: 3, hard: 2}, total: 25, goal: 5},
+    5: {difficulty: {easy: 0, medium: 3, hard: 3}, total: 30, goal: 5},
+    6: {difficulty: {easy: 0, medium: 2, hard: 4}, total: 30, goal: 5}
 }
