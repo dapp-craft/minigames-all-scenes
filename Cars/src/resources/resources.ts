bb5eec2f71f9ebdf93a694bdb2adbd7fbb1a268c
Maironire <maironire@gmail.com>
2024-09-27 16:29:14 +0300

    Cars: replace static models

--- Cars/src/resources/resources.ts
+++ Cars/src/resources/resources.ts
@@ -6,7 +6,18 @@ const imagesFolder = 'images/';
 
 
 export const STATIC_MODELS = [
-    {src: modelsFolder + 'obj_draft.gltf'},
+    {src: modelsFolder + 'obj_bench.gltf'},
+    {src: modelsFolder + 'obj_candies.gltf'},
+    {src: modelsFolder + 'obj_cars.gltf'},
+    {src: modelsFolder + 'obj_dresser.gltf'},
+    {src: modelsFolder + 'obj_floor.gltf'},
+    {src: modelsFolder + 'obj_frame.gltf'},
+    {src: modelsFolder + 'obj_grid.gltf'},
+    {src: modelsFolder + 'obj_ground.gltf'},
+    {src: modelsFolder + 'obj_lamp.gltf'},
+    {src: modelsFolder + 'obj_terminal.gltf'},
+    {src: modelsFolder + 'obj_trash.gltf'},
+    {src: modelsFolder + 'obj_wall.gltf'},
     // {src: modelsFolder + 'obj_gamezone.gltf'},
 ]
 
