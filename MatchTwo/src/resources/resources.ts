7363f3c6e0dfb4ca13e8f341f83b960e8d1f1ba8
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-09-27 13:15:18 -0300

    MatchTwo: FIX static moders ~ file formatted

--- MatchTwo/src/resources/resources.ts
+++ MatchTwo/src/resources/resources.ts
@@ -3,20 +3,17 @@ import { PBGltfContainer } from '@dcl/sdk/ecs'
 const modelsFolder = 'models/'
 const imagesFolder = 'images/'
 
-
-
 export const staticModels: PBGltfContainer[] = [
-  { src: modelsFolder +'obj_balloon.gltf' },
-  { src: modelsFolder +'obj_bench.gltf' },
-  { src: modelsFolder +'obj_door.gltf' },
-  { src: modelsFolder +'obj_fence.gltf' },
-  { src: modelsFolder +'obj_floor.gltf' },
-  { src: modelsFolder +'obj_frame.gltf' },
-  { src: modelsFolder +'obj_ground.gltf' },
-  { src: modelsFolder +'obj_terminal.gltf' },
-  { src: modelsFolder +'obj_wall.gltf' },
-  { src: modelsFolder +'obj_tower.gltf' },
-  { src: modelsFolder +'obj_gamezone.gltf' },
+  { src: modelsFolder + 'obj_balloon.gltf' },
+  { src: modelsFolder + 'obj_bench.gltf' },
+  { src: modelsFolder + 'obj_fence.gltf' },
+  { src: modelsFolder + 'obj_floor.gltf' },
+  { src: modelsFolder + 'obj_frame.gltf' },
+  { src: modelsFolder + 'obj_ground.gltf' },
+  { src: modelsFolder + 'obj_terminal.gltf' },
+  { src: modelsFolder + 'obj_wall.gltf' },
+  { src: modelsFolder + 'obj_tower.gltf' },
+  { src: modelsFolder + 'obj_gamezone.gltf' }
 ]
 
 export const tileShape: PBGltfContainer = { src: modelsFolder + 'obj_tile.gltf' }
@@ -37,16 +34,16 @@ export const toysModels: PBGltfContainer[] = [
   { src: modelsFolder + 'obj_toy13.gltf' },
   { src: modelsFolder + 'obj_toy14.gltf' },
   { src: modelsFolder + 'obj_toy15.gltf' },
-  { src: modelsFolder + 'obj_toy16.gltf' },
+  { src: modelsFolder + 'obj_toy16.gltf' }
 ]
 
 export const tileDoorShape: PBGltfContainer = { src: modelsFolder + 'obj_door.gltf' }
 export const defaulToyModel: PBGltfContainer = { src: modelsFolder + 'obj_toy01.gltf' }
 
-export const openTileSound1 = "sounds/open_1.mp3"
-export const openTileSound2 = "sounds/open_2.mp3"
-export const closeTileSound1 = "sounds/close_1.mp3"
-export const closeTileSound2 = "sounds/close_2.mp3"
-export const levelCompleteSound = "sounds/level_up.mp3"
-export const pairFoundSound = "sounds/pair_found.mp3"
+export const openTileSound1 = 'sounds/open_1.mp3'
+export const openTileSound2 = 'sounds/open_2.mp3'
+export const closeTileSound1 = 'sounds/close_1.mp3'
+export const closeTileSound2 = 'sounds/close_2.mp3'
+export const levelCompleteSound = 'sounds/level_up.mp3'
+export const pairFoundSound = 'sounds/pair_found.mp3'
 export const mainThemeSound = 'sounds/Carousel_of_Dreams.mp3'
