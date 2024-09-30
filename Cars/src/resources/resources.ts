e3b30e4aea134231a28d9691f66ed424ec867340
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-09-30 13:17:56 -0300

    Cars: added gamexone collider ~ resources formatted with prettier

--- Cars/src/resources/resources.ts
+++ Cars/src/resources/resources.ts
@@ -1,34 +1,31 @@
-import { PBGltfContainer } from "@dcl/sdk/ecs";
+import { PBGltfContainer } from '@dcl/sdk/ecs'
 
-
-const modelsFolder = 'models/';
-const imagesFolder = 'images/';
-
+const modelsFolder = 'models/'
+const imagesFolder = 'images/'
 
 export const STATIC_MODELS = [
-    {src: modelsFolder + 'obj_text.gltf'},
-    {src: modelsFolder + 'obj_bench.gltf'},
-    {src: modelsFolder + 'obj_candies.gltf'},
-    {src: modelsFolder + 'obj_cars.gltf'},
-    {src: modelsFolder + 'obj_dresser.gltf'},
-    {src: modelsFolder + 'obj_floor.gltf'},
-    {src: modelsFolder + 'obj_frame.gltf'},
-    {src: modelsFolder + 'obj_grid.gltf'},
-    {src: modelsFolder + 'obj_ground.gltf'},
-    {src: modelsFolder + 'obj_lamp.gltf'},
-    {src: modelsFolder + 'obj_terminal.gltf'},
-    {src: modelsFolder + 'obj_trash.gltf'},
-    {src: modelsFolder + 'obj_wall.gltf'},
-    // {src: modelsFolder + 'obj_gamezone.gltf'},
+  { src: modelsFolder + 'obj_text.gltf' },
+  { src: modelsFolder + 'obj_bench.gltf' },
+  { src: modelsFolder + 'obj_candies.gltf' },
+  { src: modelsFolder + 'obj_cars.gltf' },
+  { src: modelsFolder + 'obj_dresser.gltf' },
+  { src: modelsFolder + 'obj_floor.gltf' },
+  { src: modelsFolder + 'obj_frame.gltf' },
+  { src: modelsFolder + 'obj_grid.gltf' },
+  { src: modelsFolder + 'obj_ground.gltf' },
+  { src: modelsFolder + 'obj_lamp.gltf' },
+  { src: modelsFolder + 'obj_terminal.gltf' },
+  { src: modelsFolder + 'obj_trash.gltf' },
+  { src: modelsFolder + 'obj_wall.gltf' },
+  { src: modelsFolder + 'obj_gamezone.gltf' }
 ]
 
-export const boardTexture = imagesFolder + 'grid_6x6.png';
+export const boardTexture = imagesFolder + 'grid_6x6.png'
 
-export const mainCarModel = {src: modelsFolder + 'obj_car03.gltf'};
+export const mainCarModel = { src: modelsFolder + 'obj_car03.gltf' }
 export const carModels = {
-    2: {src: modelsFolder + 'obj_car02.gltf'},
-    3: {src: modelsFolder + 'obj_car01.gltf'},
+  2: { src: modelsFolder + 'obj_car02.gltf' },
+  3: { src: modelsFolder + 'obj_car01.gltf' }
 }
-
 
 export const moveCarSound = 'sounds/move_car.mp3'
