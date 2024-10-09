b1d1306855eb461ac24640e6b1e1c5b6d85b0165
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-10-08 21:37:17 -0300

    + lib init + models

--- SlidingPuzzle/src/index.ts
+++ SlidingPuzzle/src/index.ts
@@ -1,71 +1,36 @@
-// We define the empty imports so the auto-complete feature works as expected.
-import { Quaternion, Vector3 } from '@dcl/sdk/math'
-import { GltfContainer, MeshRenderer, Transform, TransformType, engine } from '@dcl/sdk/ecs'
-import { setupStaticModels } from './staticModels'
+import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
+import { GltfContainer, MeshRenderer, Transform, TransformType, engine, executeTask } from '@dcl/sdk/ecs'
 import { initGame } from './game/game'
-import { initLibrary, sceneParentEntity, ui, queue } from '@dcl-sdk/mini-games/src'
-import { syncEntity } from '@dcl/sdk/network'
 import players, { getPlayer } from '@dcl/sdk/players'
 import { GAME_ID, SESSION_DURATION } from './config'
-import { listeners } from '@dcl-sdk/mini-games/src/queue'
-import { setupUI } from './ui'
 import { TIME_LEVEL } from '@dcl-sdk/mini-games/src/ui'
+import { initMiniGame } from '../../common/library'
+import { readGltfLocators } from '../../common/locators'
+import { STATIC_MODELS } from './resources/resources'
+import { sceneParentEntity } from '@dcl-sdk/mini-games/src'
 
-initLibrary(engine, syncEntity, players, {
-  environment: 'dev',
-  gameId: GAME_ID,
-  gameTimeoutMs: SESSION_DURATION,
-  gameArea: {
-    topLeft: Vector3.create(1, 0, 0),
-    bottomRight: Vector3.create(15, 5, 9),
-    exitSpawnPoint: Vector3.create(8, 1, 13)
+const handlers = {
+  start: () => {},
+  exit: () => {},
+  restart: () => {},
+  toggleMusic: () => {},
+  toggleSfx: () => {}
+}
+
+const libraryReady = initMiniGame(
+  GAME_ID,
+  TIME_LEVEL,
+  readGltfLocators(`locators/obj_locators_default.gltf`),
+  handlers,
+  { textColor: Color4.Black(), fontSize: 3 }
+)
+
+executeTask(async () => {
+  for (const model of STATIC_MODELS) {
+    const entity = engine.addEntity()
+    GltfContainer.create(entity, model)
+    Transform.create(entity, { parent: sceneParentEntity })
   }
 })
 
-export function main() {
-  // Setup Environment models
-  setupStaticModels()
-
-  new ui.MenuButton(
-    {
-      parent: sceneParentEntity,
-      position: Vector3.create(0.008361, 1.28328, 2.94125),
-      rotation: Quaternion.fromEulerDegrees(-50, 180, 0),
-      scale: Vector3.create(1.25, 1.25, 1.25)
-    },
-    ui.uiAssets.shapes.RECT_GREEN,
-    ui.uiAssets.icons.playText,
-    'PLAY GAME',
-    () => {
-      queue.addPlayer()
-    }
-  )
-
-  const width = 2.5
-  const height = 2.8
-  const scale = 1.2
-
-  new ui.ScoreBoard(
-    {
-      parent: sceneParentEntity,
-      position: Vector3.create(-7.07, 3.02, 3.25),
-      rotation: Quaternion.fromEulerDegrees(0, -90, 0),
-      scale: Vector3.create(0.875, 0.78, 1)
-    },
-    width,
-    height,
-    scale,
-    TIME_LEVEL
-  )
-
-  queue.initQueueDisplay({
-    parent: sceneParentEntity,
-    position: Vector3.create(0, 2, 2.53653),
-    rotation: Quaternion.fromEulerDegrees(0, 0, 0),
-    scale: Vector3.create(1, 1, 1)
-  })
-
-  initGame()
-
-  setupUI()
-}
+export function main() {}
--- SlidingPuzzle/src/ui.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactEcsRenderer, UiComponent } from '@dcl/sdk/react-ecs'
-import { debugUi } from './game/debug_ui'
-
-export function setupUI() {
-  ReactEcsRenderer.setUiRenderer(uiComponent)
-}
-
-const uiComponent: UiComponent = () => [debugUi()]
