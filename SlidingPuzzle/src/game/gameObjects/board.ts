6cba849814eedf807e39f88b76f8ffcd33acea11
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-10-11 02:55:30 -0300

    preview image logic added

--- SlidingPuzzle/src/game/gameObjects/board.ts
+++ SlidingPuzzle/src/game/gameObjects/board.ts
@@ -8,7 +8,7 @@ export function initBoard() {
   Transform.create(BOARD, BOARD_TRANSFORM)
 }
 
-const BOARD_TRANSFORM: TransformType = {
+export const BOARD_TRANSFORM: TransformType = {
   position: { x: 8, y: 2.6636881828308105, z: 1.0992899895 },
   scale: { x: 1, y: 1, z: 1 },
   rotation: Quaternion.fromAngleAxis(180, Vector3.create(0, 1, 0))
---
+++ SlidingPuzzle/src/game/prevoewImage.ts
@@ -0,0 +1,64 @@
+import {
+  Animator,
+  Billboard,
+  EasingFunction,
+  Entity,
+  GltfContainer,
+  InputAction,
+  Material,
+  MaterialTransparencyMode,
+  MeshCollider,
+  MeshRenderer,
+  TextureFilterMode,
+  Transform,
+  TransformType,
+  Tween,
+  Vector3Type,
+  VisibilityComponent,
+  engine,
+  pointerEventsSystem
+} from '@dcl/sdk/ecs'
+import { Color4, Matrix, Quaternion, Vector3 } from '@dcl/sdk/math'
+import { syncEntity } from '@dcl/sdk/network'
+import { BOARD, BOARD_TRANSFORM } from './gameObjects/board'
+
+const PREVIEW_IMAGE_TRANSFORM = BOARD_TRANSFORM
+PREVIEW_IMAGE_TRANSFORM.position.z -= 0.1
+
+const previewImageEntity = engine.addEntity()
+Transform.create(previewImageEntity, PREVIEW_IMAGE_TRANSFORM)
+MeshRenderer.setPlane(previewImageEntity)
+syncEntity(previewImageEntity, [Material.componentId, VisibilityComponent.componentId], 5000 + 100)
+
+export function showPreviewImage(image: string) {
+  VisibilityComponent.createOrReplace(this.imageEntity, { visible: true })
+  Material.createOrReplace(this.imageEntity, {
+    material: {
+      $case: 'pbr',
+      pbr: {
+        texture: {
+          tex: {
+            $case: 'texture',
+            texture: { src: image, filterMode: TextureFilterMode.TFM_TRILINEAR }
+          }
+        },
+        emissiveColor: Color4.White(),
+        emissiveIntensity: 0.9,
+        emissiveTexture: {
+          tex: {
+            $case: 'texture',
+            texture: { src: image, filterMode: TextureFilterMode.TFM_TRILINEAR }
+          }
+        },
+        roughness: 1.0,
+        specularIntensity: 0,
+        metallic: 0,
+        transparencyMode: MaterialTransparencyMode.MTM_AUTO
+      }
+    }
+  })
+}
+
+export function hidePreviewImage() {
+  VisibilityComponent.createOrReplace(this.imageEntity, { visible: false })
+}
