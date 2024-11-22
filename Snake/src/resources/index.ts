d4fefa960a530c308eb8b25a169fe8417dbca920
Evgenii Baldin <evgenii.baldin0@gmail.com>
2024-11-21 21:59:52 -0400

    Snake: model update

--- Snake/src/resources/index.ts
+++ Snake/src/resources/index.ts
@@ -5,15 +5,14 @@ const imagesFolder = 'images/'
 const soundsFolder = 'sounds/'
 
 export const STATIC_MODELS = [
-  // { src: modelsFolder + 'obj_floor.gltf' },
+  { src: modelsFolder + 'obj_floor.gltf' },
   { src: modelsFolder + 'obj_frame.gltf' },
   { src: modelsFolder + 'obj_gamezone.gltf' },
-  // { src: modelsFolder + 'obj_ground.gltf' },
+  { src: modelsFolder + 'obj_ground.gltf' },
   { src: modelsFolder + 'obj_railings.gltf' },
-  { src: modelsFolder + 'obj_snake01.gltf' },
-  { src: modelsFolder + 'obj_snake02.gltf' },
-  // { src: modelsFolder + 'obj_terminal.gltf' },
+  { src: modelsFolder + 'obj_terminal.gltf' },
   { src: modelsFolder + 'obj_text.gltf' },
+  { src: modelsFolder + 'obj_sofa.gltf' },
   { src: modelsFolder + 'obj_wall.gltf' }
 ]
 
