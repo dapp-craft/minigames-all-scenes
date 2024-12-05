454ada9ca2307bf5ef5dde2a51ad00a0f8bf5c7c
vaTka <vatkawork@gmail.com>
2024-12-05 15:36:14 +0100

    Toads: fix

--- Toads/src/game/game.ts
+++ Toads/src/game/game.ts
@@ -28,7 +28,7 @@ export function getReadyToStart() {
   console.log('Get Ready to start!')
   playButton.disable()
   soundManager.playSound('enterSounds', soundConfig.volume)
-  startTimeOut != undefined && utils.timers.clearTimeout(startTimeOut)
+  startTimeOut && utils.timers.clearTimeout(startTimeOut)
   exitCallback(false)
   CameraModeArea.createOrReplace(engine.PlayerEntity, {
     area: Vector3.create(1, 1, 1),
