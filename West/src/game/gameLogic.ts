2d9a9010b66ea15816ab6bc968ed958412acf774
vaTka <vatkawork@gmail.com>
2024-12-11 14:00:51 +0100

    West: updates + Add levels ~ change player level system ~ refactor randomizator ~ change finish round logic

--- West/src/game/gameLogic.ts
+++ West/src/game/gameLogic.ts
@@ -1,26 +1,26 @@
 import * as utils from '@dcl-sdk/utils'
-import { EasingFunction, Entity, InputAction, pointerEventsSystem, TextShape, Transform, Tween } from "@dcl/sdk/ecs";
+import { EasingFunction, Entity, InputAction, Material, pointerEventsSystem, TextShape, Transform, Tween } from "@dcl/sdk/ecs";
 import { tempLocators, westGameConfig } from "../config";
 import { westGameState } from "../state";
-import { Quaternion, Vector3 } from "@dcl/sdk/math";
+import { Color4, Quaternion, Vector3 } from "@dcl/sdk/math";
 import { levels } from '../levels';
 
 export class GameLogic {
     private timeoutByEntity: Map<Entity, utils.TimerId> = new Map()
     private endRoundTimeout = 0
-    private playerLevel = 0
+    private playerLevel = 1
     private playerHP = westGameConfig.playerMaxHP
     private targetData = new Map()
     private playerScore = 0
 
     public startGame() {
-        // this.spawnRandomizer('row', 5)
+        // this.stopGame()
+        this.resetData()
         this.playGame()
     }
 
     private playGame() {
         this.targetData.clear()
-        this.setPlayerLevel()
         const levelData = levels.get(this.playerLevel)
         const remainingArray = [...levelData!.role];
         this.endRoundTimeout = utils.timers.setTimeout(() => this.finishRound(), levelData!.stayTime + levelData!.appearanceTime * 2 + 100)
@@ -28,10 +28,11 @@ export class GameLogic {
         for (let i = 0; i < levelData!.targetAmount; i++) {
             Tween.deleteFrom(westGameState.availableEntity[i])
             Transform.createOrReplace(westGameState.availableEntity[i], tempLocators.get(`obj_locator_${targetPositionArray![i]}`))
-            this.spawnEntity(westGameState.availableEntity[i])
             const randomIndex = Math.floor(Math.random() * remainingArray.length);
             const chosenRole = remainingArray[randomIndex]
             remainingArray.splice(randomIndex, 1);
+            this.setTexture(westGameState.availableEntity[i], chosenRole)
+            this.spawnEntity(westGameState.availableEntity[i])
             this.targetData.set(i, { entity: westGameState.availableEntity[i], enemy: chosenRole, dead: false })
             pointerEventsSystem.onPointerDown(
                 {
@@ -41,13 +42,13 @@ export class GameLogic {
                 () => {
                     this.targetData.get(i).dead = true
                     console.log(westGameState.availableEntity[i], ' is clicked, he was Bandit? ', this.targetData.get(i).enemy)
-                    Tween.deleteFrom(westGameState.availableEntity[i])
                     utils.timers.clearTimeout(this.timeoutByEntity.get(westGameState.availableEntity[i])!)
+                    Tween.deleteFrom(westGameState.availableEntity[i])
                     pointerEventsSystem.removeOnPointerDown(westGameState.availableEntity[i])
                     utils.timers.setTimeout(() => this.hitEntity(westGameState.availableEntity[i]), 10)
                     this.targetData.get(i).enemy ? this.playerScore += 10 : this.playerScore -= 10
                     this.updateCounters()
-                    !this.isEnemyLeft() && this.stopRound()
+                    this.isEnemyLeft()
                 }
             )
         }
@@ -55,45 +56,80 @@ export class GameLogic {
 
     // TO DO: REFACTOR
     private spawnRandomizer(spawnType: string, entityAmount: number) {
-        console.log("spawnRandomizer TYPE: ", spawnType)
+        console.log("spawnRandomizer TYPE: ", spawnType);
 
-        const generator = (firstRow: boolean, gap: number, entityAmountInRow: number = entityAmount) => {
+        const generator = ({ firstRow, gap = 1, entityAmountInRow = entityAmount }: { firstRow: boolean, gap?: number | "random", entityAmountInRow?: number }) => {
+            if (gap === "random") {
+                gap = (Math.random() < 0.5 || entityAmountInRow >= 3) ? 2 : 3
+            }
+
             const start = entityAmountInRow < 3 ? Math.floor(Math.random() * 5) + 1 : 1
-            let sequence = [start]
+            const sequence: number[] = [start]
             let increasing = true
+
             for (let i = 1; i < entityAmountInRow; i++) {
-                let prev = sequence[i - 1]
+                const prev = sequence[i - 1]
                 let next
+
                 if (increasing) {
-                    next = prev + gap;
+                    next = prev + gap
                     if (next > 5) {
                         increasing = false
                         next = start - gap
                     }
-                } else next = prev - gap
+                } else {
+                    next = prev - gap
+                }
+
                 sequence.push(next)
             }
-            if (!firstRow) sequence = sequence.map(element => element + 5);
+
+            if (!firstRow) {
+                sequence.forEach((_, i) => sequence[i] += 5)
+            }
+
             console.log(sequence)
             return sequence
         }
+        console.log("TYPE: ", spawnType)
+        switch (spawnType) {
+            case 'row':
+                return generator({ firstRow: true })
 
-        if (spawnType == 'row') {
-            return generator(true, 1)
-        } else if (spawnType == 'gapRow') {
-            return generator(true, 2)
-        } else if (spawnType == 'twoLevels') {
-            const firstLevel = Math.floor(Math.random() * 2) == 0 ? true : false
-            let entityAmountInRow
-            const odd = entityAmount % 2 === 0 ? false : true
-            if (odd) entityAmountInRow = Math.ceil(entityAmount / 2)
-            else entityAmountInRow = entityAmount / 2
-            const firstArray = generator(firstLevel, 2, odd ? entityAmountInRow - 1 : entityAmountInRow)
-            const secondArray = generator(!firstLevel, 2, entityAmountInRow)
-            const response = firstArray.concat(secondArray)
-            console.log('Concat: ', response)
-            return response
+            case 'gapRow':
+                return generator({ firstRow: true, gap: 'random' })
+
+            case 'twoLevels': {
+                const firstLevel = Math.random() < 0.5
+                const odd = entityAmount % 2 !== 0
+                const entityAmountInRow = Math.ceil(entityAmount / 2)
+
+                const firstArray = generator({
+                    firstRow: firstLevel,
+                    gap: 'random',
+                    entityAmountInRow: odd ? entityAmountInRow - 1 : entityAmountInRow
+                })
+
+                const secondArray = generator({
+                    firstRow: !firstLevel,
+                    gap: 'random',
+                    entityAmountInRow
+                })
+
+                const response = [...firstArray, ...secondArray]
+                console.log('Concat: ', response)
+                return response
+            }
+
+            default:
+                throw new Error(`Unknown spawnType: ${spawnType}`)
         }
+    }
+
+    private setTexture(entity: Entity, bandit: boolean) {
+        Material.setPbrMaterial(entity, {
+            albedoColor: bandit ? Color4.Red() : Color4.Blue(),
+        })
     }
 
     private spawnEntity(entity: Entity) {
@@ -114,15 +150,20 @@ export class GameLogic {
                 end: Quaternion.fromEulerDegrees(-90, 1, 1),
 
             }),
-            duration: levels.get(this.playerLevel)!.appearanceTime,
+            duration: levels.get(this.playerLevel)!.appearanceTime / 4,
             easingFunction: EasingFunction.EF_EASEINBACK,
         });
     }
 
-    private isEnemyLeft() {
+    private async isEnemyLeft() {
         let enemyLeftCounter = 0
         this.targetData.forEach(data => { if (data.enemy && !data.dead) enemyLeftCounter++ })
-        return enemyLeftCounter == 0 ? false : true
+        const enemyLeft = enemyLeftCounter == 0
+        if (enemyLeft) {
+            this.setPlayerLevel()
+            await this.stopRound()
+            this.playGame()
+        }
     }
 
     private hitPlayer() {
@@ -139,17 +180,21 @@ export class GameLogic {
         TextShape.getMutable(playerScoreText).text = `Score \n${this.playerScore}`
     }
 
-    private finishRound() {
+    private async finishRound() {
         console.log("Lose ROUND")
         this.targetData.forEach(data => {
             console.log(data)
             if (!data.dead && data.enemy) this.hitPlayer()
         })
-        this.stopRound()
+        if (this.playerHP <= 0) return
+        await this.stopRound()
+        this.playGame()
     }
 
-    private stopRound(exit: boolean = false) {
+    private async stopRound(noAnimation: boolean = false) {
         console.log("Stop ROUND")
+        let resolveReady!: () => void
+        let RoundIsStopped = new Promise((res: any) => { resolveReady = res })
         utils.timers.clearTimeout(this.endRoundTimeout)
         for (let i = 0; i < levels.get(this.playerLevel)!.targetAmount; i++) {
             utils.timers.clearTimeout(this.timeoutByEntity.get(westGameState.availableEntity[i])!)
@@ -159,18 +204,26 @@ export class GameLogic {
             utils.timers.setTimeout(() => {
                 Tween.deleteFrom(westGameState.availableEntity[i])
                 Transform.createOrReplace(westGameState.availableEntity[i], { position: Vector3.create(1, 1, 2), rotation: Quaternion.Zero() })
-            }, !exit ? levels.get(this.playerLevel)!.appearanceTime + 100 : 1)
+                resolveReady()
+            }, !noAnimation ? levels.get(this.playerLevel)!.appearanceTime + 100 : 1)
         }
+        if (noAnimation) return
+        await RoundIsStopped;
     }
 
     private setPlayerLevel() {
-        this.playerLevel = 1 // TEMP
+        // TEMP
+        if (this.playerLevel >= levels.size) {
+            this.stopGame()
+            return console.log("GAME OVER!")
+        }
+        this.playerLevel++
         console.log("Player Level: ", this.playerLevel)
     }
 
     private resetData() {
         this.endRoundTimeout = 0
-        this.playerLevel = 0
+        this.playerLevel = 1
         this.playerHP = westGameConfig.playerMaxHP
         this.playerScore = 0
         this.updateCounters()
--- West/src/levels.ts
+++ West/src/levels.ts
@@ -1,16 +1,27 @@
 type LevelData = {
-    speed: number;
-    floor: number;
     targetAmount: number;
     appearanceTime: number;
     stayTime: number;
-    role: Boolean[];
+    role: boolean[];
     generationType: string
 }
 
 type LevelsMap = Map<number, LevelData>;
 
 export const levels: LevelsMap = new Map([
-    [1, { speed: 1000, floor: 1, targetAmount: 5, appearanceTime: 500, stayTime: 5000, role: [true, false], generationType: 'twoLevels' }],
-    [2, { speed: 1000, floor: 1, targetAmount: 3, appearanceTime: 500, stayTime: 5000, role: [true, false, true], generationType: 'row' }]
+    [1, { targetAmount: 2, appearanceTime: 2000, stayTime: 2000, role: [true, false], generationType: 'row' }],
+    [2, { targetAmount: 2, appearanceTime: 2000, stayTime: 2000, role: [true, false], generationType: 'gapRow' }],
+    [3, { targetAmount: 2, appearanceTime: 2000, stayTime: 2000, role: [true, false], generationType: 'twoLevels' }],
+    [4, { targetAmount: 3, appearanceTime: 1900, stayTime: 1900, role: [true, false, false], generationType: 'row' }],
+    [5, { targetAmount: 3, appearanceTime: 1900, stayTime: 1900, role: [true, false, true], generationType: 'row' }],
+    [6, { targetAmount: 3, appearanceTime: 1900, stayTime: 1900, role: [true, false, false], generationType: 'gapRow' }],
+    [7, { targetAmount: 3, appearanceTime: 1800, stayTime: 1800, role: [true, false, true], generationType: 'gapRow' }],
+    [8, { targetAmount: 3, appearanceTime: 1800, stayTime: 1800, role: [true, false, false], generationType: 'twoLevels' }],
+    [9, { targetAmount: 3, appearanceTime: 1800, stayTime: 1800, role: [true, false, true], generationType: 'twoLevels' }],
+    [10, { targetAmount: 4, appearanceTime: 1700, stayTime: 1700, role: [true, false, false, false], generationType: 'twoLevels' }],
+    [11, { targetAmount: 4, appearanceTime: 1700, stayTime: 1700, role: [true, false, true, false], generationType: 'twoLevels' }],
+    [12, { targetAmount: 4, appearanceTime: 1700, stayTime: 1700, role: [true, false, true, true], generationType: 'twoLevels' }],
+    [13, { targetAmount: 5, appearanceTime: 1600, stayTime: 1600, role: [true, false, false, false, false], generationType: 'twoLevels' }],
+    [14, { targetAmount: 5, appearanceTime: 1600, stayTime: 1600, role: [true, false, true, false, false], generationType: 'twoLevels' }],
+    [15, { targetAmount: 5, appearanceTime: 1600, stayTime: 1600, role: [true, false, true, true, false], generationType: 'twoLevels' }],
 ])
