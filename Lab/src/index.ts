f2beefd6f322fc7f30988a1034e96979b577b5cd
Maironire <maironire@gmail.com>
2024-12-27 16:49:37 +0300

    Lab: minimalize anomaly corrector

--- Lab/src/index.ts
+++ Lab/src/index.ts
@@ -16,44 +16,16 @@ import { SoundManager } from './game/soundManager'
 
 (globalThis as any).DEBUG_NETWORK_MESSAGES = false
 
-// ERRATA: This component and system are used to track and block entity id reuse,
+// ERRATA: This piece of code is used to track and block entity id reuse,
 // because sometimes it causes wierd problems like 'Transform already exists',
 // even tho the entity id was obtained literally in previous line of code
-const RewrittenID = engine.defineComponent('rewrittenID', {
-    isNew: Schemas.Boolean,
-    timestamp: Schemas.Number,
-    originalID: Schemas.Number
-})
 const addEntity = engine.addEntity
-const STATE_NAMES = ['Unknown', 'UsedEntity', 'Removed', 'Reserved']
-
-function logEntityData(id: Entity) {
-    const data = [...engine.componentsIter()].reduce(
-        (a, c) => (c.has(id) ? { ...a, [c.componentName]: c.get(id) } : a),
-        {}
-    )
-    console.log(`Entity ${id} (${STATE_NAMES[engine.getEntityState(id)]})`, data)
-}
-
 engine.addEntity = () => {
-    let id = addEntity()
-    if (id > 65535) {
-        const trashed = [id]
-        while ((id = addEntity()) > 65535) trashed.push(id)
-        RewrittenID.create(id, { isNew: true, timestamp: Date.now(), originalID: trashed[0] })
-        // console.log(`Trashed ids:`, trashed)
-    }
+    let id
+    do id = addEntity()
+    while (id > 65535)
     return id
 }
-
-engine.addSystem(() => {
-    for (let [entity, { isNew, originalID }] of engine.getEntitiesWith(RewrittenID)) {
-        if (!isNew) continue
-        // console.error(`NEW REWRITTEN ENTITY, id=${originalID}, corrected=${entity}`)
-        // logEntityData(entity)
-        RewrittenID.getMutable(entity).isNew = false
-    }
-}, 0)
 
 const Synchronizer = CreateStateSynchronizer(
     'Lab',
