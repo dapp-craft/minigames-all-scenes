09a9e7dc9cf8dce983cdfcc3f84cbd9244d82fe4
Maironire <maironire@gmail.com>
2024-12-27 19:49:34 +0300

    Snake: tune speed progression

--- Snake/src/game/config.ts
+++ Snake/src/game/config.ts
@@ -1,4 +1,4 @@
 
 
-export const SPEED = [0.6, 0.5, 0.4, 0.35, 0.3, 0.25, 0.20, 0.15, 0.10, 0.05, 0.04] // Itervals between moves in seconds
-export const SPEED_INCREASE_CHECKPOINTS = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330] // Checkpoints in seconds to increase speed 
+export const SPEED = [0.6, 0.5, 0.4, 0.35, 0.3, 0.25, 0.20, 0.15, 0.11, 0.08, 0.06, 0.05, 0.04, 0.03] // Itervals between moves in seconds
+export const SPEED_INCREASE_CHECKPOINTS = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390] // Checkpoints in seconds to increase speed 
