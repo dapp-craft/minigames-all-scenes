da160f2fb0f3d9aba09642e655d86ce1477cf521
Evgenii Baldin <evgenii.baldin0@gmail.com>
2025-02-05 22:47:30 -0500

    Snake: speed update

--- Snake/src/game/config.ts
+++ Snake/src/game/config.ts
@@ -1,5 +1,5 @@
 // Intervals between moves in seconds (decreasing as speed increases)
-export const UPDATE_INTERVALS = [0.6, 0.5, 0.4, 0.35, 0.3, 0.25, 0.20, 0.15, 0.11, 0.08, 0.06, 0.05, 0.04, 0.03] 
+export const UPDATE_INTERVALS = [0.6, 0.5, 0.425, 0.35, 0.3, 0.25, 0.20, 0.15, 0.11, 0.08, 0.06, 0.05, 0.04, 0.03] 
 
 // Checkpoints in seconds to increase speed 
 export const UPDATE_INTERVALS_CHECKPOINTS = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390] 
