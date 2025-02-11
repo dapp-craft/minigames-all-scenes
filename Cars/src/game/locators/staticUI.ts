1a838e43f2112ff7149d5acce3220029dc34b9af
Evgenii Baldin <evgenii.baldin0@gmail.com>
2025-02-10 19:59:34 -0500

    Cars: static UI locator file update

--- Cars/src/game/locators/staticUI.ts
+++ Cars/src/game/locators/staticUI.ts
@@ -14,7 +14,7 @@ export const staticUIPositions: {
 }
 
 export async function readStaticUIPositions() {
-  const staticUILocators = await readGltfLocators('locators/obj_locators_default.gltf')
+  const staticUILocators = await readGltfLocators('locators/obj_locators_unique.gltf')
 
   staticUIPositions.label_easy = staticUILocators.get('label_easy') as TransformType
   staticUIPositions.label_medium = staticUILocators.get('label_medium') as TransformType
@@ -23,4 +23,6 @@ export async function readStaticUIPositions() {
   staticUIPositions.label_easy.position = Vector3.add(staticUIPositions.label_easy.position, Vector3.create(8, 0, 8))
   staticUIPositions.label_medium.position = Vector3.add(staticUIPositions.label_medium.position, Vector3.create(8, 0, 8))
   staticUIPositions.label_hard.position = Vector3.add(staticUIPositions.label_hard.position, Vector3.create(8, 0, 8))
+
+  console.log("Static locators", staticUILocators)
 }
